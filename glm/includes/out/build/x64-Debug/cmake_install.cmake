60ae22a7ed4a035ec43f4887fe86ffaf703545ce
Julius <56944305+qrrtdang@users.noreply.github.com>
2022-02-14 12:08:18 +0800

    debuging
    h

--- glm/includes/out/build/x64-Debug/cmake_install.cmake
+++ glm/includes/out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Xie Loong/source/repos/SP42019VS/glm/includes
+# Install script for directory: C:/Users/Julius/source/repos/SP4/glm/includes
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/Xie Loong/Source/Repos/SP42019VS/glm/includes/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/Julius/Source/Repos/SP4/glm/includes/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Xie Loong/source/repos/SP42019VS/glm/includes/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Julius/source/repos/SP4/glm/includes/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
